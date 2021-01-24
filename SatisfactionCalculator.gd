b3b744de82655412e95014a2c029b3238b174e14
carlostgit <carlos.h.corredera@gmail.com>
2021-01-24 19:10:01 +0100

    Saco parte de la lógica de SatisfactionCalculator.gd a SatisfactionCurve.gd

--- SatisfactionCalculator.gd
+++ SatisfactionCalculator.gd
@@ -15,28 +15,46 @@ var _param_combo_maximum_quantity_satisf = {"sweets":3.0}
 var _products = ["chocolate","candy"]
 
 const Plotter = preload("res://Plotter.gd")
-
+const SatisfactionCurve = preload("res://SatisfactionCurve.gd")
+#TODO Usar SatisfactionCurve
+var _product_satisf_curve_dict:Dictionary = {}
+var _combo_satisf_curve_dict:Dictionary = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 
-	var test_combination:Dictionary = {"chocolate": 2, "candy": 2}	
+#	var test_combination:Dictionary = {"chocolate": 2, "candy": 2}	
 #	print(calculate_satisf(test_combination))
-	
-	var plotter:Plotter = Plotter.new(1,1)
+
+#	var param_max_satisf:float = 10
+#	var param_quantity:float = 10
+#	var param_satisf_at_1:float = 5
+#	var preference_at_0 = calculate_param_preference_at_0(param_max_satisf, param_satisf_at_1, param_quantity)
+#	_param_preference_at_0["chocolate"] = preference_at_0
+#	_maximum_satisf["chocolate"] = param_max_satisf
+
+	var plotter:Plotter = Plotter.new(100,15)
 	plotter.set_size(Vector2(300,300))
 	plotter.updated_size()
-	var dimin_ret_fact_func_ref:FuncRef = funcref( self, "get_diminishing_returns_factor")
-	plotter.add_func_ref(dimin_ret_fact_func_ref,"dimin_ret_fact")
+#	var dimin_ret_fact_func_ref:FuncRef = funcref( self, "get_diminishing_returns_factor")
+#	plotter.add_func_ref(dimin_ret_fact_func_ref,"dimin_ret_fact")
 	self.call_deferred("add_child",plotter)
+	
+#	print(calculate_param_preference_at_0(10, 1, 1))
+
+	var satis_curve_chocolate:SatisfactionCurve = SatisfactionCurve.new(2.16,10)
+	var satis_curve_candy:SatisfactionCurve = SatisfactionCurve.new(10,4)
+	var satis_curve_sweets:SatisfactionCurve = SatisfactionCurve.new(10.8,3)
+	
+	_product_satisf_curve_dict["chocolate"]=satis_curve_chocolate
+	_product_satisf_curve_dict["candy"]=satis_curve_candy
+	_combo_satisf_curve_dict["sweets"]=satis_curve_sweets
 	
 	plotter.add_func_ref(funcref( self, "calculate_satifaction_of_chocolate"),"chocolate")
 	plotter.add_func_ref(funcref( self, "calculate_satifaction_of_candy"),"candy")
 	plotter.add_func_ref(funcref( self, "calculate_satifaction_of_sweets"),"sweets")
 
-	print(calculate_param_preference_at_0(10, 1, 1))
-
-
+	
 ##test funcs
 func calculate_satifaction_of_chocolate(quantity_arg:float) -> float:
 	return calculate_satifaction_of_product("chocolate",quantity_arg)
@@ -58,6 +76,8 @@ func calculate_satisf(combination_dict_arg:Dictionary) -> float:
 	for product in self._products:
 		var amount_of_product = combination_dict_arg[product]
 		satisf_of_prod_individually += self.calculate_satifaction_of_product(product,amount_of_product)
+#		var satisf_curve:SatisfactionCurve = _product_satisf_curve_dict[product]
+#		satisf_of_prod_individually += satisf_curve.calculate_satifaction(amount_of_product)
 
 	var satisf_of_combi = 0.0
 	for combi_name in self._combo.keys():
@@ -140,10 +160,13 @@ func calculate_satifaction_of_product(product_arg:String, quantity_arg:float) ->
 		return 0.0
 	
 	var ret_satisf = 0.0
-	var pref_at_0 = _param_preference_at_0[product_arg]
-	var max_satisf = _maximum_satisf[product_arg]
+#	var pref_at_0 = _param_preference_at_0[product_arg]
+#	var max_satisf = _maximum_satisf[product_arg]
+#
+#	ret_satisf = max_satisf*get_diminishing_returns_factor(quantity_arg*pref_at_0/max_satisf)
 	
-	ret_satisf = max_satisf*get_diminishing_returns_factor(quantity_arg*pref_at_0/max_satisf)
+	var product_satisf_curve:SatisfactionCurve = self._product_satisf_curve_dict[product_arg]
+	ret_satisf = product_satisf_curve.calculate_satifaction(quantity_arg)
 	
 	return ret_satisf
 
@@ -153,35 +176,36 @@ func calculate_satifaction_of_prod_combo(combi_arg:String, quantity_arg:float) -
 		return 0.0
 	
 	var ret_satisf = 0.0
-	var pref_at_0 = self._param_combo_preference_at_0[combi_arg]
-	var max_satisf = self._param_combo_maximum_quantity_satisf[combi_arg]
-	
-	ret_satisf = max_satisf*get_diminishing_returns_factor(quantity_arg*pref_at_0/max_satisf)
+#	var pref_at_0 = self._param_combo_preference_at_0[combi_arg]
+#	var max_satisf = self._param_combo_maximum_quantity_satisf[combi_arg]
+#
+#	ret_satisf = max_satisf*get_diminishing_returns_factor(quantity_arg*pref_at_0/max_satisf)
+#
+	var combo_satisf_curve:SatisfactionCurve = self._combo_satisf_curve_dict[combi_arg]
+	ret_satisf = combo_satisf_curve.calculate_satifaction(quantity_arg)
 	
 	return ret_satisf
 
-func get_diminishing_returns_factor(quantity_arg:float) -> float:
-	#Voy a llamar al termino "1-(1/(0.25*x+1)^2)" Diminishing Returns Factor
-	#Esta ecuación tendría un máximo en 1, y tendría pendiente 1 en 0
-	#Es como una ecuación y = x, pero que se va haciendo más y más horizontal hasta q ya no crece la y
-	var result = 0.0
-	var denominator_square_root = 0.25*quantity_arg + 1.0;
-	var denominator = denominator_square_root*denominator_square_root
-	result = 1.0 - (1.0/denominator)
-	if result < 0:
-		result = 0
-	return result
+#func get_diminishing_returns_factor(quantity_arg:float) -> float:
+#	#Voy a llamar al termino "1-(1/(0.25*x+1)^2)" Diminishing Returns Factor
+#	#Esta ecuación tendría un máximo en 1, y tendría pendiente 1 en 0
+#	#Es como una ecuación y = x, pero que se va haciendo más y más horizontal hasta q ya no crece la y
+#	var result = 0.0
+#	var denominator_square_root = 0.25*quantity_arg + 1.0;
+#	var denominator = denominator_square_root*denominator_square_root
+#	result = 1.0 - (1.0/denominator)
+#	if result < 0:
+#		result = 0
+#	return result
 
-static func calculate_param_preference_at_0(max_satisfaction_arg:float, param_point_satisfaction_arg:float, param_point_quantity_arg:float)->float:
-	#
-	
-	var coefficient_q:float = 0.25*param_point_quantity_arg/max_satisfaction_arg
-	var coefficient_z:float = param_point_satisfaction_arg-max_satisfaction_arg
-	var coefficient_qq:float = coefficient_q*coefficient_q
-
-	var coefficient_sqrt:float = sqrt(-4*coefficient_qq*max_satisfaction_arg/coefficient_z)
-	
-
-	var preference_at_0:float = (-2*coefficient_q+coefficient_sqrt)/(2*coefficient_qq)
-
-	return preference_at_0
+#static func calculate_param_preference_at_0(max_satisfaction_arg:float, param_point_satisfaction_arg:float, param_point_quantity_arg:float)->float:
+#	#
+#	var coefficient_q:float = 0.25*param_point_quantity_arg/max_satisfaction_arg
+#	var coefficient_z:float = param_point_satisfaction_arg-max_satisfaction_arg
+#	var coefficient_qq:float = coefficient_q*coefficient_q
+#
+#	var coefficient_sqrt:float = sqrt(-4*coefficient_qq*max_satisfaction_arg/coefficient_z)
+#
+#	var preference_at_0:float = (-2*coefficient_q+coefficient_sqrt)/(2*coefficient_qq)
+#
+#	return preference_at_0
---
+++ SatisfactionCurve.gd
@@ -0,0 +1,78 @@
+extends Control
+
+# Declare member variables here. Examples:
+# var a = 2
+# var b = "text"
+
+var _param_preference_at_0 = 2.16
+var _maximum_satisf = 10.0
+
+const Plotter = preload("res://Plotter.gd")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+
+#	var test_combination:Dictionary = {"chocolate": 2, "candy": 2}	
+#	print(calculate_satisf(test_combination))
+
+#	var param_max_satisf:float = 10
+#	var param_quantity:float = 10
+#	var param_satisf_at_1:float = 5
+#	var preference_at_0 = calculate_param_preference_at_0(param_max_satisf, param_satisf_at_1, param_quantity)
+#
+#	_param_preference_at_0 = preference_at_0
+#	_maximum_satisf = param_max_satisf
+#
+#	var plotter:Plotter = Plotter.new(100,10)
+#	plotter.set_size(Vector2(300,300))
+#	plotter.updated_size()
+#	var dimin_ret_fact_func_ref:FuncRef = funcref( self, "get_diminishing_returns_factor")
+#	plotter.add_func_ref(dimin_ret_fact_func_ref,"dimin_ret_fact")
+#	self.call_deferred("add_child",plotter)
+#	plotter.add_func_ref(funcref( self, "calculate_satifaction"),"satisfaction")	
+
+#	print(calculate_param_preference_at_0(10, 1, 1))
+	pass
+	
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+#	pass
+
+func _init(param_preference_at_0_arg:float = 1, maximum_satisf_arg:float = 1):
+	_param_preference_at_0 = param_preference_at_0_arg
+	_maximum_satisf = maximum_satisf_arg
+
+
+func get_diminishing_returns_factor(quantity_arg:float) -> float:
+	#Voy a llamar al termino "1-(1/(0.25*x+1)^2)" Diminishing Returns Factor
+	#Esta ecuación tendría un máximo en 1, y tendría pendiente 1 en 0
+	#Es como una ecuación y = x, pero que se va haciendo más y más horizontal hasta q ya no crece la y
+	var result = 0.0
+	var denominator_square_root = 0.25*quantity_arg + 1.0;
+	var denominator = denominator_square_root*denominator_square_root
+	result = 1.0 - (1.0/denominator)
+	if result < 0:
+		result = 0
+	return result
+
+static func calculate_param_preference_at_0(max_satisfaction_arg:float, param_point_satisfaction_arg:float, param_point_quantity_arg:float)->float:
+	#
+	var coefficient_q:float = 0.25*param_point_quantity_arg/max_satisfaction_arg
+	var coefficient_z:float = param_point_satisfaction_arg-max_satisfaction_arg
+	var coefficient_qq:float = coefficient_q*coefficient_q
+
+	var coefficient_sqrt:float = sqrt(-4*coefficient_qq*max_satisfaction_arg/coefficient_z)
+	
+	var preference_at_0:float = (-2*coefficient_q+coefficient_sqrt)/(2*coefficient_qq)
+
+	return preference_at_0
+
+func calculate_satifaction(quantity_arg:float) -> float:
+		
+	var ret_satisf = 0.0
+	var pref_at_0 = _param_preference_at_0
+	var max_satisf = _maximum_satisf
+	
+	ret_satisf = max_satisf*get_diminishing_returns_factor(quantity_arg*pref_at_0/max_satisf)
+	
+	return ret_satisf
