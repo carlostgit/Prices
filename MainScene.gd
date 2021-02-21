b2b8e7c139c3c80a958df948ce5fa9bf3ba8628f
carlostgit <carlos.h.corredera@gmail.com>
2021-02-21 03:26:57 +0100

    Reordeno controles en MainScene

--- MainScene.gd
+++ MainScene.gd
@@ -28,7 +28,7 @@ func _ready():
 #	pass
 
 func init()->void:
-	$OwnedItems.init_with_combidict(_owned_items_dict,"owned",["",""])
+	$OwnedItems/CombinationItem.init_with_combidict(_owned_items_dict,"owned",["",""])
 	_satisfaction_calculator = create_satisfaction_calculator()
 	_trade_calculator = TradeCalculator.new(_satisfaction_calculator)
 	
@@ -72,13 +72,13 @@ func update_ranking_of_preferences()->void:
 #
 #	$CombinationSatisfaction.init(combination_dict_array_sorted,combination_satisfaction)
 #	TODO: combination_price_list, meterlo en $CombinationSatisfaction
-	$CombinationSatisfaction.init(combination_satisfaction_list, combination_price_list, "Ranking of preferences")
+	$RankingOfPreferences/CombinationSatisfaction.init(combination_satisfaction_list, combination_price_list, "Ranking of preferences")
 
 func update_of_owned_combination()->void:
 		#Satisfaction of owned combination
-	var owned_combination:Combination = $OwnedItems.get_combination()
+	var owned_combination:Combination = $OwnedItems/CombinationItem.get_combination()
 	var owned_color:Color = Color( 1, 0.5, 0.31, 1 ) 
-	$CombinationSatisfaction.highlight_combination_with_color(owned_combination,owned_color)
+	$RankingOfPreferences/CombinationSatisfaction.highlight_combination_with_color(owned_combination,owned_color)
 	var satisf_of_owned:float = _satisfaction_calculator.calculate_satisf_of_combination(owned_combination)
 	$OwnedItems/LabelSatisfaction.set_text("Satisfaction of owned: " + String(satisf_of_owned).pad_decimals(2))
 
@@ -95,6 +95,7 @@ func update_prices()->void:
 #	combination_prices.init_with_combidict(Prices.get_combidict(),"Prices",[str(Prices.get_combidict())])
 #	combination_prices.set_position(Vector2(300,100))
 #	self.add_child(combination_prices)
+	$Prices/LabelCurrency.set_text("Currency: "+Prices.get_currency())
 	$Prices/LabelPrices.set_text("Prices: "+str(Prices.get_combidict()))
 	var candy_price:float = Prices.get_amount_of_product("candy")
 	var chocolate_price:float = Prices.get_amount_of_product("chocolate")
@@ -107,15 +108,16 @@ func update_best_combination()->void:
 	var best_combidict:Dictionary = _trade_calculator.calculate_best_combidict(get_value_of_owned_combination())
 	var best_combination:Combination = Combination.new(best_combidict)
 	var best_color:Color = Color( 0, 0.5, 0.5, 1 ) 
-	$CombinationSatisfaction.highlight_combination_with_color(best_combination,best_color)
+	$RankingOfPreferences/CombinationSatisfaction.highlight_combination_with_color(best_combination,best_color)
 	$BestCombination/LabelBestCombination.set_text("Best combination: " + str(best_combidict))
 	var satisf_of_best_combi:float = _satisfaction_calculator.calculate_satisf_of_combination(best_combination)
 	$BestCombination/LabelSatisfaction.set_text("Satisfaction of best combi: " + String(satisf_of_best_combi).pad_decimals(2))
 	var value_of_best_combi:float = Prices.calculate_combination_price(best_combination)
+	$BestCombination/LabelCost.set_text("Cost: "+ String(value_of_best_combi).pad_decimals(2)+"$")
 
 func update_trade()->void:
 	var trade_combidict:Dictionary = _trade_calculator.calculate_trade_for_combidict(_owned_items_dict)
-	$LabelTrade.set_text("Trade: "+str(trade_combidict))
+	$Trade/LabelTrade.set_text("Trade: "+str(trade_combidict))
 
 
 
